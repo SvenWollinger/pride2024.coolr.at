ccef7f4fbbff1c641b9b8e9193a24714df5d3ff7
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2024-06-09 15:29:37 +0200

    Initial commit

---
+++ src/main/kotlin/io/wollinger/colors/Main.kt
@@ -0,0 +1,7 @@
+import kotlinx.browser.document
+import org.w3c.dom.HTMLParagraphElement
+
+fun main() {
+    val text = document.createElement("p") as HTMLParagraphElement
+    text.innerText = "Hello Pride 2024!"
+}
